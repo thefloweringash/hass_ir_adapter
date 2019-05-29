dc5df02f759bac54684c40a5efd4003a40d031b3
Andrew Childs <lorne@cons.org.nz>
2019-05-29 10:06:19 +0900

    nix: use go 1.11 for consistent modSha256

--- nix/package.nix
+++ nix/package.nix
@@ -1,6 +1,7 @@
-{ lib, buildGoModule }:
+{ lib, buildGo111Module }:
 
-buildGoModule {
+# Also builds with buildGo112Module, but with a different modSha256.
+buildGo111Module {
   name = "hass_ir_adapter";
   src = lib.sourceFilesBySuffices ./.. [ ".go" ".mod" ".sum" ];
   modSha256 = "1lvkl2ay0i21vwywnai8hl3nm1pb3hibpg2r4bq8608pg5km4kxa";
