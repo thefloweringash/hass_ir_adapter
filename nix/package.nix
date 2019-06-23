428aafa743df528901af89c63bf8bd7c586408b3
Andrew Childs <lorne@cons.org.nz>
2019-06-24 02:54:51 +0900

    [WIP] restrict nix binaries to main package

--- nix/package.nix
+++ nix/package.nix
@@ -5,5 +5,6 @@ buildGo111Module {
   name = "hass_ir_adapter";
   src = lib.sourceFilesBySuffices ./.. [ ".go" ".mod" ".sum" ];
   modSha256 = "1zj90zvbn258sykl1kdh57y1y34vis0ygjjaicvs60m3v24ig2wf";
+  subPackages = [ "." ];
   doCheck = true;
 }
