cbd02252b6d1392faef491ffff662b8a0ed0fb28
Andrew Childs <lorne@cons.org.nz>
2020-06-22 12:44:23 +0900

    nix: migrate from modSha256 to vendorSha256
    Upstream nixpkgs is migrating to vendorSha256. modSha256 is no longer
    available in nixos-unstable and support for vendorSha256 has been
    backported to nixos-20.03.

--- nix/package.nix
+++ nix/package.nix
@@ -1,21 +1,9 @@
 { lib, buildGoModule }:
 
-let
-  goVersion = lib.versions.majorMinor
-    (buildGoModule {src = ""; modSha256 = ""; pname = ""; version="";}).go.version;
-
-  modSha256 = {
-    "1.11" = "1zj90zvbn258sykl1kdh57y1y34vis0ygjjaicvs60m3v24ig2wf";
-    "1.12" = "02ppjvaz5lkz0afwbcrf8xk986bs88dqzm0aphbkq0x9jdv7dhxr";
-    "1.13" = "02ppjvaz5lkz0afwbcrf8xk986bs88dqzm0aphbkq0x9jdv7dhxr";
-    "1.14" = "02ppjvaz5lkz0afwbcrf8xk986bs88dqzm0aphbkq0x9jdv7dhxr";
-  }.${goVersion} or (throw "Missing modSha256 for go version ${goVersion}");
-in
-
 buildGoModule {
   name = "hass_ir_adapter";
   src = lib.sourceFilesBySuffices ./.. [ ".go" ".mod" ".sum" ];
-  inherit modSha256;
+  vendorSha256 = "1v9cdj4py769dn7q7pmp9kfs6h08kab8sc2pfkrsc9ll2dnngxdj";
   subPackages = [ "." ];
   doCheck = true;
 }
