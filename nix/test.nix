26cc0980a078078ced7c34fed5cce487b7b2f50a
Andrew Childs <lorne@cons.org.nz>
2020-06-24 02:49:41 +0900

    Add tasmota to integration tests and examples

--- nix/test.nix
+++ nix/test.nix
@@ -29,13 +29,16 @@ import (<nixpkgs> + "/nixos/tests/make-test.nix") ({ pkgs, lib, ... }:
     $machine->execute('mosquitto_sub -t "ir/#" -F "%t -> %x" | logger -t mqtt-ir & disown');
 
     $machine->execute('mosquitto_sub -t ir/ESP_1/send -F %x | tee /tmp/esp1.log &');
+    $machine->execute('mosquitto_sub -t ir/tasmota/cmnd/IRhvac -F %p | tee /tmp/tasmota.log &');
     $machine->execute('mosquitto_sub -t homeassistant/climate/living_room_ac/state -F %p | tee /tmp/climate.log &');
 
     $machine->execute('mosquitto_pub -t homeassistant/climate/living_room_ac/set_mode -m cool');
+    $machine->execute('mosquitto_pub -t homeassistant/climate/tasmota_ac/set_mode -m cool');
 
     $machine->sleep(5);
 
     $machine->succeed('set -x && test "$(cat /tmp/esp1.log)" == 010ff123cb26010024030d000000000049');
+    $machine->succeed('set -x && test "$(jq -r --slurp "last | .Vendor" < /tmp/tasmota.log)" == HITACHI_AC424');
     $machine->succeed('set -x && test "$(jq -r --slurp "last | .mode" < /tmp/climate.log)" == cool');
   '';
 })
